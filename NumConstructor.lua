95b464e6bfc87611a1a3d0e7bd1374b3f6e6effc
Sam <74118063+Reid910@users.noreply.github.com>
2022-06-22 16:05:20 -0700

    Update NumConstructor.lua

--- NumConstructor.lua
+++ NumConstructor.lua
@@ -3,13 +3,26 @@ local MathLib = require(game:GetService("ServerStorage").MathLib)
 local num = {}
 num.__index = num
 
-function num.New()
+function NewStat(T,N)
+	local S = Instance.new(T)
+	S.Name = N
+	return S
+end
+
+function num.New(StatName,Parent)
 	
 	local NewNumber = {}
 	setmetatable(NewNumber,num)
 	
 	NewNumber.N = 0
 	NewNumber.Notation = 0
+	
+	if StatName then
+		local NumbValue = NewStat("NumberValue",StatName or "") -- creating a numbervalue
+		NumbValue.Value = NewNumber.N
+		NumbValue:SetAttribute("Not",NewNumber.Notation)
+		NumbValue.Parent = Parent
+	end
 	
 	return NewNumber
 	
@@ -22,6 +35,14 @@ function num.Temp(a,b)
 	return {N=a,Notation=b}
 end
 
+function num:Update(a,b)
+	self.N,self.Notation = a,b
+	if self.NValue then
+		self.NValue.Value = a
+		self.NValue:SetAttribute("Not",b)
+	end
+end
+
 function num:Set(a,b)
 	
 	self.N = a
@@ -30,24 +51,49 @@ function num:Set(a,b)
 	return self
 end
 
-function num:Add(onum) -- other number
-	return MathLib.AddNumbers(self.N,self.Notation,onum.N,onum.Notation)
+function num:Add(onum,Setvalue) -- other number
+	local a,b = MathLib.AddNumbers(self.N,self.Notation,onum.N,onum.Notation)
+	if Setvalue then
+		self:Update(a,b)
+	else
+		return a,b
+	end
 end
 
-function num:Sub(onum) -- other number
-	return MathLib.SubNumbers(self.N,self.Notation,onum.N,onum.Notation)
+function num:Sub(onum,Setvalue)
+	local a,b = MathLib.SubNumbers(self.N,self.Notation,onum.N,onum.Notation)
+	if Setvalue then
+		self:Update(a,b)
+	else
+		return a,b
+	end
 end
 
-function num:Multiply(onum) -- other number
-	return MathLib.MultiplyNumbers(self.N,self.Notation,onum.N,onum.Notation)
+function num:Multiply(onum,Setvalue)
+	local a,b = MathLib.MultiplyNumbers(self.N,self.Notation,onum.N,onum.Notation)
+	if Setvalue then
+		self:Update(a,b)
+	else
+		return a,b
+	end
 end
 
-function num:Divide(onum) -- other number
-	return MathLib.DivideNumbers(self.N,self.Notation,onum.N,onum.Notation)
+function num:Divide(onum,Setvalue)
+	local a,b = MathLib.DivideNumbers(self.N,self.Notation,onum.N,onum.Notation)
+	if Setvalue then
+		self:Update(a,b)
+	else
+		return a,b
+	end
 end
 
-function num:Exp(onum) -- other number
-	return MathLib.Pow(self.N,self.Notation,onum.N,onum.Notation)
+function num:Exp(onum,Setvalue)
+	local a,b = MathLib.Pow(self.N,self.Notation,onum.N,onum.Notation)
+	if Setvalue then
+		self:Update(a,b)
+	else
+		return a,b
+	end
 end
 
 return num
