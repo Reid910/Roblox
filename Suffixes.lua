863ee581633258e4f565595345d9c205b45d2424
Sam <74118063+Reid910@users.noreply.github.com>
2022-11-09 16:47:03 -0800

    Update Suffixes.lua

--- Suffixes.lua
+++ Suffixes.lua
@@ -1,21 +1,18 @@
--- you can consider using this
 
-local abbreviations = {"", "K", "M", "B", "T", "Qd", "Qn", "Sx", "Sp", "Oc", "No", "Dc", "Uc",}
+local Suffixes = {"", "K", "M", "B", "T", "Qd", "Qn", "Sx", "Sp", "Oc", "No", "Dc", "Uc",}
 
-return function(value,decimals)
+return function(value,DV)
 	
-	if not value or value == 0 then
+	if not value or value < 1 then
 		return 0
 	end
 	
-	decimals = decimals or value >= 1000 and 2 or 0
+	DV = DV or value >= 1000 and 2 or 0
 	
-	local suffix = math.floor(math.log(value,1000))
+	local SN = math.floor(math.log(value,1000))
 	
-	local normal = value / math.pow(1000,suffix)
+	local Front = value / math.pow(1000,SN)
 	
-	return string.format("%." .. decimals .. "f",normal) .. abbreviations[suffix + 1] or ("e+"..(suffix*3)) -- 5.4 K | 8.92 T
+	return string.format("%." .. DV .. "f",Front) .. Suffixes[SN + 1] or ("e+"..(SN*3)) -- 5.4 K | 8.92 T
 	
 end
-
--- this is how i would layout the module
