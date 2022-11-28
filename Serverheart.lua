258747297f5e657c6ec65001190c3d80911f438e
Sam <74118063+Reid910@users.noreply.github.com>
2022-11-27 19:01:33 -0800

    Update Serverheart.lua

--- Serverheart.lua
+++ Serverheart.lua
@@ -1,48 +1,31 @@
-local m = {
-	Heartfunctions = {},
-	Permanentfunctions = {},
-	Erroredfunctions = {}
-}
-
-function m.AddToHeart(func,NeverBreak)
-	if NeverBreak then
-		table.insert(m.Permanentfunctions,func)
-	else
-		table.insert(m.Heartfunctions,func)
-	end
-end
+local Perma,r_true = {},{} -- permanent functions, functions that return true
 
 game:GetService("RunService").Heartbeat:Connect(function(Delta)
-	for i = 1,#m.Permanentfunctions do local f = m.Permanentfunctions[i]
-		local s,e = pcall(f,Delta)
+	local s,e,self
+	for i = 1,#Perma do self = Perma[i]
+		s,e = pcall(self._function,Delta)
+	end
+	for i = #r_true,1,-1 do self = r_true[i]
+		s,e = pcall(self._function,Delta)
 		if not s then
-			warn(e.."\n-- -- | Serverheart Permanent |")
-		end
-	end
-	for i = #m.Heartfunctions,1,-1 do local f = m.Heartfunctions[i]
-		local s,e = pcall(f,Delta)
-		if s and e then -- if success and returned true then del
-			table.remove(m.Heartfunctions,i)
-		elseif not s then
-			table.insert(m.Erroredfunctions,{["f"]=f,["c"]=1})
-			table.remove(m.Heartfunctions,i)
-		end
-	end
-	for i = #m.Erroredfunctions,1,-1 do local f = m.Erroredfunctions[i]
-		local s,e = pcall(f.f,Delta)
-		if not s then -- if the function errored again
-			f.c += 1
-			if f.c > 5 then -- if the error count is greater then 5 then del
-				warn(e.."\n-- -- | Serverheart Errorfunctions |")
-				table.remove(m.Erroredfunctions,i)
+			warn(e.."\n-- -- | Serverheart return-true |")
+			r_true[i].C += 1
+			if r_true[i].C > 5 then
+				table.remove(r_true,i)
 			end
-		elseif s and e then -- if success and returned true then del
-			table.remove(m.Erroredfunctions,i)
-		elseif s then -- It successfully ran add it back
-			table.insert(m.Heartfunctions,f.f)
-			table.remove(m.Erroredfunctions,i)
+		else
+			r_true[i].C = 0
 		end
 	end
 end)
 
-return m
+return function(_function,Permanent)
+	local self = {}
+	self._function = _function
+	if Permanent then
+		table.insert(Perma,_function)
+	else
+		self.C = 0
+		table.insert(r_true,_function)
+	end
+end
